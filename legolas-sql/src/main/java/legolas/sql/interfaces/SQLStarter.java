ef7a9769427f4ec48de7cd573654f3efc2b0a879
Braz <anderson.braz@gmail.com>
2020-09-24 15:55:35 -0300

    Username with special chars should be replaced with underline (_)

--- legolas-sql/src/main/java/legolas/sql/interfaces/SQLStarter.java
+++ legolas-sql/src/main/java/legolas/sql/interfaces/SQLStarter.java
@@ -3,10 +3,14 @@ package legolas.sql.interfaces;
 import legolas.docker.interfaces.DockerStarter;
 import org.testcontainers.containers.GenericContainer;
 
+import java.util.regex.Pattern;
+
 public abstract class SQLStarter<C extends GenericContainer> extends DockerStarter<C> {
+  private static final Pattern usernamePattern = Pattern.compile("[^A-Za-z0-9]");
 
   protected String username() {
-    return System.getProperty("user.name");
+    String username =  System.getProperty("user.name");
+    return usernamePattern.matcher(username).replaceAll("_").trim();
   }
 
   protected abstract TargetDatabase targetDatabase();
